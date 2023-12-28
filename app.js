b9a6dfdebcec326eb9019a328d97ea7431b90b9c
edgarsuarez97@gmail.com <edgarsuarez97@gmail.com>
2023-12-28 12:12:42 -0400

    last changes, streaming ready

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 const { fork } = require("child_process");
 var fileUpload = require('express-fileupload');
+const fs = require("fs");
+
 const path = require('path');
 
 var app = express();
 
 app.use(express.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'))
+});
 
 app.post('/upload',
     fileUpload({ createParentPath:true }), 
@@ -30,7 +36,49 @@ app.post('/upload',
         
         // return res.json({status: 'success', message: Object.keys(files).toString() })
     }
-  );
+);
+
+app.get('/stream', function(req, res) {
+        var filepath = path.join(__dirname,'files')
+        const files = fs.readdirSync(filepath)
+        for(var i in files) {
+            if(files[i] !== null && files[i] !== '') {
+                filepath = path.join(__dirname,'files',files[i]);
+                break;
+            }
+        }
+        const stat = fs.statSync(filepath)
+        const fileSize = stat.size
+        const range = req.headers.range
+        
+        if (range) {
+        const parts = range.replace(/bytes=/, "").split("-")
+        const start = parseInt(parts[0], 10)
+        const end = parts[1]
+        ? parseInt(parts[1], 10)
+        : fileSize-1
+        
+        const chunksize = (end-start)+1
+        const file = fs.createReadStream(filepath, {start, end})
+        const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+        }
+        
+        res.writeHead(206, head)
+        file.pipe(res)
+        } else {
+        const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+        }
+        res.writeHead(200, head)
+        fs.createReadStream(filepath).pipe(res)
+        }
+    }
+);
 
 const runApp=(port)=>{
     app.listen(port);
